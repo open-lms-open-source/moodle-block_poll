00f4bd24e5ed20d7dcea7a2f95ad5cbcd523b3d9
Adam Olley <adam.olley@netspot.com.au>
2012-02-10 13:41:17 +1030

    lang: Add german language translation. Credit to Florian Wetzel!

---
+++ lang/de/block_poll.php
@@ -0,0 +1,26 @@
+<?php
+$string['choose'] = 'Wähle...';
+$string['config_param'] = 'Parameter';
+$string['config_value'] = 'Wert';
+$string['editanonymousresponses'] = 'Anonym';
+$string['edit:poll'] = 'Darf Abstimmungsblöcke bearbeiten';
+$string['editblocktitle'] = 'Blocktitel';
+$string['editmaxbarwidth'] = 'Maximale Breite des Graphen';
+$string['editpollname'] = 'Name der Abstimmung';
+$string['editpollquestion'] = 'Frage';
+$string['editpolleligible'] = 'Abstimmungsberechtigte Personen';
+$string['editpolloptions'] = 'Anzahl der Antwortmöglichkeiten';
+$string['formaltitle'] = 'Abstimmung';
+$string['nopollsavailable'] = 'Aktuell sind keine Abstimmungen verfügbar, wähle den untenstehenden Link Erstelle/Bearbeite Abstimmung, um eine neue Abstimmung zu erstellen';
+$string['option'] = 'Option';
+$string['pluginname'] = 'Abstimmung';
+$string['poll:editpoll'] = 'Abstimmungen bearbeiten und verwalten';
+$string['pollconfirmdelete'] = 'Sicher, dass die Abstimmung ({$a}) und alle Antworten gelöscht werden soll?';
+$string['pollwarning'] = 'Nur Lehrer können diese Aktion ausführen';
+$string['responses'] = 'Antworten';
+$string['submit'] = 'Bestätigen';
+$string['tabconfigblock'] = 'Block bearbeiten';
+$string['tabeditpoll'] = 'Erstelle/Bearbeite Abstimmung';
+$string['tabmanagepolls'] = 'Abstimmungen verwalten';
+$string['tabresponses'] = 'Zu den Antworten';
+$string['useranonymous'] = 'Anonym';
