2d6af19a8cedc95d4aff1a091f0ec4b952e098c1
Adam Olley <adam.olley@blackboard.com>
2017-05-18 15:51:07 +0930

    fix version bump - we now required M3.1 or better

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2017051601;
-$plugin->requires = 2015051100;
+$plugin->version = 2017051800;
+$plugin->requires = 2016052300;
 $plugin->component = 'block_poll';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '2.9.0.0';
+$plugin->release   = '3.1.0.1';
