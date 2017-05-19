23a94ba5f9edd2563ec87669366e83b7bba4f8ba
Adam Olley <adam.olley@blackboard.com>
2017-05-19 10:00:36 +0930

    simple typo in phpdoc

--- classes/output/renderer.php
+++ classes/output/renderer.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Poll block rendrer
+ * Poll block renderer
  *
  * @package    block_poll
  * @copyright  2017 Adam Olley <adam.olley@blackboard.com>
